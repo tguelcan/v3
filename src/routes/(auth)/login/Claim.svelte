1cada6861072d7b671d90b2cffa699f8e50437ef
tayfunguelcan <tayfun.guelcan@rpc-partners.com>
2025-11-30 23:09:29 +0100

    styling: fix colors

--- src/routes/(auth)/login/Claim.svelte
+++ src/routes/(auth)/login/Claim.svelte
@@ -2,7 +2,7 @@
 	let { class: className } = $props();
 </script>
 
-<div class="md:col-span-2 card dark:text-base-content text-base-100 {className}">
+<div class="md:col-span-2 card text-base-content dark:text-base-100 {className}">
 	<h1 class="text-4xl font-serif">Get Started with us</h1>
 	<p>A secure place to share knowledge with others...</p>
 </div>
